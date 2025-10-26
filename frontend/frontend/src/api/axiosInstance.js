c01a0a24a93246432e3603e3e929ca120c73b360
Jeevitha <ravikumarjeevitha254@gmail.com>
2025-10-27 00:52:06 +0530

    Update axiosInstance.js

--- frontend/frontend/src/api/axiosInstance.js
+++ frontend/frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://sixs-consulting-proj-1.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://sixs-consulting-proj-4.onrender.com/api",
   withCredentials: true,
 });
 

cce191dbbb24e5d2958bea528049f79741d29fdc
Jeevitha <ravikumarjeevitha254@gmail.com>
2025-10-26 23:58:32 +0530

    Update CORS origin for frontend URL

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://sixs-consulting-proj-3.onrender.com",
+    origin: "https://sixs-consulting-proj-frontend-1.onrender.com",
     credentials: true,
   })
 );
